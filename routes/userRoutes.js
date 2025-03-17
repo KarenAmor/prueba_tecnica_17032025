da98504951a70ae690790645a572e708593f96e4
KarenMoreno <karedimor@gmail.com>
2025-03-17 16:47:18 -0500

    Se agrego documentacion mediante swagger para validacion de endpoints y modelo

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,72 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 
-// Crear un usuario
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Usuario:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: ID único del usuario
+ *         nombre:
+ *           type: string
+ *           description: Nombre del usuario
+ *         email:
+ *           type: string
+ *           description: Email del usuario
+ *         edad:
+ *           type: number
+ *           description: Edad del usuario
+ *         direcciones:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Direccion'
+ *         fecha_creacion:
+ *           type: string
+ *           format: date-time
+ *           description: Fecha de creación del usuario
+ *     Direccion:
+ *       type: object
+ *       properties:
+ *         calle:
+ *           type: string
+ *           description: Calle de la dirección
+ *         ciudad:
+ *           type: string
+ *           description: Ciudad de la dirección
+ *         pais:
+ *           type: string
+ *           description: País de la dirección
+ *         codigo_postal:
+ *           type: string
+ *           description: Código postal de la dirección
+ */
+
+/**
+ * @swagger
+ * /usuarios:
+ *   post:
+ *     summary: Crea un nuevo usuario
+ *     description: Crea un nuevo usuario en la base de datos.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Usuario'
+ *     responses:
+ *       201:
+ *         description: Usuario creado correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuario'
+ *       400:
+ *         description: Error en la solicitud
+ */
 router.post('/usuarios', async (req, res) => {
   try {
     const user = new User(req.body);
@@ -13,81 +78,210 @@ router.post('/usuarios', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /usuarios:
+ *   get:
+ *     summary: Obtiene la lista de usuarios
+ *     description: Retorna una lista de todos los usuarios registrados.
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: Número de página para la paginación
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: Número de usuarios por página
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios obtenida correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Usuario'
+ *       500:
+ *         description: Error al obtener los usuarios
+ */
 router.get('/usuarios', async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-  
-    try {
-      const users = await User.find().skip(skip).limit(limit);
-      res.send(users);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-// Buscar usuarios por ciudad
+  try {
+    const users = await User.find().skip(skip).limit(limit);
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/**
+ * @swagger
+ * /usuarios/buscar:
+ *   get:
+ *     summary: Busca usuarios por ciudad
+ *     description: Retorna una lista de usuarios que tienen una dirección en la ciudad especificada.
+ *     parameters:
+ *       - in: query
+ *         name: ciudad
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Ciudad para buscar usuarios
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios encontrados
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Usuario'
+ *       500:
+ *         description: Error al buscar usuarios
+ */
 router.get('/usuarios/buscar', async (req, res) => {
-    try {
-      const ciudad = req.query.ciudad;
-      const users = await User.find({ 'direcciones.ciudad': ciudad });
-      res.send(users);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
+  try {
+    const ciudad = req.query.ciudad;
+    const users = await User.find({ 'direcciones.ciudad': ciudad });
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-// Obtener un usuario por ID
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   get:
+ *     summary: Obtiene un usuario por su ID
+ *     description: Retorna un usuario específico basado en su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del usuario a buscar
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Usuario encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuario'
+ *       404:
+ *         description: Usuario no encontrado
+ *       500:
+ *         description: Error al buscar el usuario
+ */
 router.get('/usuarios/:id', async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      if (!user) {
-        return res.status(404).send({ message: 'Usuario no encontrado' }); // Mensaje personalizado
-      }
-      res.send(user);
-    } catch (error) {
-      res.status(500).send({ message: 'Error al buscar el usuario', error: error.message }); // Mensaje de error detallado
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ message: 'Usuario no encontrado' });
     }
-  });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ message: 'Error al buscar el usuario', error: error.message });
+  }
+});
 
-// Actualizar un usuario por ID
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   put:
+ *     summary: Actualiza un usuario por su ID
+ *     description: Actualiza un usuario específico basado en su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del usuario a actualizar
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Usuario'
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuario'
+ *       404:
+ *         description: Usuario no encontrado
+ *       400:
+ *         description: Error al actualizar el usuario
+ */
 router.put('/usuarios/:id', async (req, res) => {
-    try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      if (!user) {
-        return res.status(404).send({ message: 'Usuario no encontrado' });
-      }
-  
-      // Agrega un mensaje a la respuesta
-      const response = {
-        ...user.toObject(), // Convierte el documento de Mongoose a un objeto plano
-        mensaje: 'Usuario modificado correctamente', // Mensaje adicional
-      };
-  
-      res.send(response);
-    } catch (error) {
-      res.status(400).send({ message: 'Error al actualizar el usuario', error: error.message });
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).send({ message: 'Usuario no encontrado' });
     }
-  });
 
-// Eliminar un usuario por ID
+    // Agrega un mensaje a la respuesta
+    const response = {
+      ...user.toObject(), // Convierte el documento de Mongoose a un objeto plano
+      mensaje: 'Usuario modificado correctamente', // Mensaje adicional
+    };
+
+    res.send(response);
+  } catch (error) {
+    res.status(400).send({ message: 'Error al actualizar el usuario', error: error.message });
+  }
+});
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   delete:
+ *     summary: Elimina un usuario por su ID
+ *     description: Elimina un usuario específico basado en su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del usuario a eliminar
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuario'
+ *       404:
+ *         description: Usuario no encontrado
+ *       500:
+ *         description: Error al eliminar el usuario
+ */
 router.delete('/usuarios/:id', async (req, res) => {
-    try {
-      const user = await User.findByIdAndDelete(req.params.id);
-      if (!user) {
-        return res.status(404).send({ message: 'Usuario no encontrado' });
-      }
-  
-      // Devuelve un mensaje de éxito junto con el usuario eliminado
-      res.send({
-        message: 'Usuario eliminado correctamente',
-        usuario: user, // Opcional: incluir el usuario eliminado en la respuesta
-      });
-    } catch (error) {
-      res.status(500).send({ message: 'Error al eliminar el usuario', error: error.message });
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send({ message: 'Usuario no encontrado' });
     }
-  });
 
-
+    // Devuelve un mensaje de éxito junto con el usuario eliminado
+    res.send({
+      message: 'Usuario eliminado correctamente',
+      usuario: user, // Opcional: incluir el usuario eliminado en la respuesta
+    });
+  } catch (error) {
+    res.status(500).send({ message: 'Error al eliminar el usuario', error: error.message });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,38 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 dotenv.config();
 
 const app = express();
+
+// Configuración de Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Prueba Backend',
+      version: '1.0.0',
+      description: 'Documentación de la API para la prueba técnica de backend',
+      contact: {
+        name: 'Tu Nombre',
+        email: 'tu@email.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', // Cambia esto según tu entorno
+        description: 'Servidor local',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Ruta a los archivos donde están tus rutas
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,4 +48,5 @@ app.use(userRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log(`Documentación de la API en http://localhost:${port}/api-docs`); // Usa "port" en lugar de "PORT"
 });
